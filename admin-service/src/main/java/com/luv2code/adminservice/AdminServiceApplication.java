3c5e800244fbaf06c9f6ab3a1a3601d5051aad05
linuxminhat <dangnhatminhdhbk@gmail.com>
2025-10-13 21:10:37 +0700

    adding-admin-service-modules

---
+++ admin-service/src/main/java/com/luv2code/adminservice/AdminServiceApplication.java
@@ -0,0 +1,15 @@
+package com.luv2code.adminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableTransactionManagement
+public class AdminServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServiceApplication.class, args);
+    }
+}
